433bb9ba34f5baf2b5d69b4c13dfaca995021976
Your Name01~ <you@example.com>
2022-08-23 15:51:23 -0500

    Proyecto lavanderia

---
+++ lavanderia/src/test/java/com/lavanderia/lavanderia/LavanderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanderia.lavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
